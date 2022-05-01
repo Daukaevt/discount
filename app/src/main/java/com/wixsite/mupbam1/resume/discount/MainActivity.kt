60ee406759f182cd11dc0c1d8ce14579e6851758
Daukaevt <mupbam1@gmail.com>
2022-05-01 12:01:18 +0300

    interactive

--- app/src/main/java/com/wixsite/mupbam1/resume/discount/MainActivity.kt
+++ app/src/main/java/com/wixsite/mupbam1/resume/discount/MainActivity.kt
@@ -3,34 +3,72 @@ package com.wixsite.mupbam1.resume.discount
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.wixsite.mupbam1.resume.discount.databinding.ActivityMainBinding
 
-val priceData= listOf<Int>(5,100,20,66,16)
+//val priceData= listOf<Int>(5,100,20,66,16)
+val priceData= mutableListOf<Int>()
 val resultData= mutableListOf<Int>()
 val discount=50
-val offset=1
-val readLength=3
+var offset=0
+var readLength=0
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.apply {
+            btRun.setOnClickListener {
+                if (edOffset.text.isNotEmpty()&&edReadLength.text.isNotEmpty()) {
+                    offset = edOffset.text.toString().toInt()
+                    readLength = edReadLength.text.toString().toInt()
+                    edPrice.visibility = View.VISIBLE
+                    tvArray.visibility = View.VISIBLE
+                    tvResult.visibility = View.VISIBLE
+                    btEnter.visibility = View.VISIBLE
+                    btRun.visibility=View.GONE
+                    edOffset.visibility=View.GONE
+                    edReadLength.visibility=View.GONE
 
-        discountPriceData()
-        Log.d("MyLog","resultData-$resultData")
+                    discountPriceData()
+
+                }
+            }
+        }
     }
 
     private fun discountPriceData() {
-        val position=offset+ readLength-1
+        binding.apply {
 
-        if (priceData.size>=position){
-            for (index in offset..position){
-                var discountPrice=priceData[index]* discount/100
+            btEnter.setOnClickListener {
+                if (binding.edPrice.text.isNotEmpty()){
+                    priceData.add(edPrice.text.toString().toInt())
+                    tvArray.text = priceData.toString()
 
-                resultData.add(discountPrice)
+                    val position = offset + readLength - 1
+
+                    when (priceData.size)
+                    {   in 0..offset-> Log.d("MyLog","more")
+                        in offset+1..position+1-> {
+
+                            var discountPrice = priceData[priceData.size-1] * discount / 100
+                            resultData.add(discountPrice)
+                            Log.d("MyLog", " resultData    $resultData")
+
+                        }
+                        else->    Log.d("MyLog","no more")
+                    }
+
+                    binding.tvResult.text = resultData.toString()
+                    edPrice.getText().clear()
+                }
+
 
             }
         }
-
     }
 
 }
